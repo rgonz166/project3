cd156ccd3911f4b2b2c6c8284749c65debbca5bc
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-11-14 20:02:49 -0800

    Auth0 + DB_seeds + Pulling from DB to List

---
+++ client/src/App/pages/List.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import API from "../utils/API";
+
+class List extends Component {
+  // Initialize the state
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    }
+  }
+
+  // Fetch the list on first mount
+  componentDidMount() {
+    this.getList();
+  }
+
+  // Retrieves the list of items from the Express app
+  getList = () => {
+    API.getVendors()
+      .then(res => {
+        this.setState({ list: res.data });
+      }
+      )
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { list } = this.state;
+
+    return (
+      <div className="App">
+        <h1>List of Items</h1>
+        {/* Check to see if any items are found*/}
+        {list.length ? (
+          <div>
+            {/* Render the list of items */}
+            {list.map((item) => {
+              return (
+                <div key={item._id}>
+                  {item.storeName}
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+            <div>
+              <h2>No List Items Found</h2>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default List;
--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-/* eslint-disable no-console */
-const express = require("express");
-const { join } = require("path");
-const morgan = require("morgan");
+require("dotenv").config();
+const express = require('express');
+const { join } = require('path');
+const morgan = require('morgan');
+const mongoose = require("mongoose");
+const routes = require("./utils");
 const app = express();
 
-const port = process.env.SERVER_PORT || 3000;
-
 app.use(morgan("dev"));
-app.use(express.static(join(__dirname, "build")));
+// Serve the static files from the React app
+app.use(express.static(join(__dirname, 'client/build')));
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use((_, res) => {
-  res.sendFile(join(__dirname, "build", "index.html"));
-});
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Routes
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/vendorlist");
+
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
