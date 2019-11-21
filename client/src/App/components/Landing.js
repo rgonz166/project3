c10b59074f997e49b43c64b0bdbd003a3a38c050
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-11-20 18:26:38 -0800

    Link added to Landing page

--- client/src/App/components/Landing.js
+++ client/src/App/components/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { useAuth0 } from "../../react-auth0-spa";
+import { Link } from "react-router-dom";
 import { Button } from 'reactstrap';
 import logo from "../assets/food-truck.jpg";
 
@@ -7,33 +8,35 @@ const Landing = () => {
 
     const { loginWithRedirect } = useAuth0();
 
-    
-        return (
-            <div>
-                <div className="text-center hero my-2">
-                    <img className="mb-2 app-logo" src={logo} alt="logo" width="120" />
-                    <h1 className="mb-2">MHEELS</h1>
-                    <p className="lead">
+
+    return (
+        <div>
+            <div className="text-center hero my-2">
+                <img className="mb-2 app-logo" src={logo} alt="logo" width="120" />
+                <h1 className="mb-2">MHEELS</h1>
+                <p className="lead">
                     Track and Eat
                      </p>
-                 </div>
-                <div className="container">
+            </div>
+            <div className="container">
                 <div className="row">
                     <div className="col-5 text-right">
-                    <Button color="success"
-                    onClick={() => loginWithRedirect({})}
-                    >Vendor</Button>
+                        <Button color="success"
+                            onClick={() => loginWithRedirect({})}
+                        >Vendor</Button>
                     </div>
                     <div className="col-2 text-center">
-                    <p>OR</p>
+                        <p>OR</p>
                     </div>
                     <div className="col-5">
-                    <Button color="info">Customer</Button>
+                        <Link to="/customer">
+                            <Button color="info">Customer</Button>
+                        </Link>
                     </div>
                 </div>
-             </div>
+            </div>
         </div>
-        )
+    )
 }
 
 export default Landing;
