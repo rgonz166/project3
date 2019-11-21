4f8969e32599e72279a10d5c7911297129708014
Mitu Fikir <hannatefera2@gmail.com>
2019-11-20 18:11:29 -0800

    Modified style for business form, search bar and customer page

--- client/src/App/pages/BusinessInfo.js
+++ client/src/App/pages/BusinessInfo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, Row, Col, Container} from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Row, Col, Container, Card, CardBody, CardHeader} from 'reactstrap';
 
  class BusinessInfo extends Component {
      constructor(props){
@@ -19,65 +19,69 @@ import { Button, Form, FormGroup, Label, Input, Row, Col, Container} from 'react
     render() {
         const { } = this.props;
         return (
-            <Container>
+            <Container className="mb-3">
                 <Row>
-                    <Col sm="12" md={{ size: 6, offset: 3 }}>
-                        <Form>
-                        <Label>Business Information</Label>
-                            <FormGroup>
-                                <Input 
-                                type="text" 
-                                name="contact" 
-                                id="contact" 
-                                placeholder="Owner" 
-                                
-                                />
-                            </FormGroup>
-                             <FormGroup>
-                                <Input 
-                                type="text" 
-                                name="name" 
-                                id="business-name" 
-                                placeholder="Business name"                                
-                                />
-                            </FormGroup>
-                            <FormGroup>
-                                <Input 
-                                type="text" 
-                                name="city" 
-                                placeholder="City (optional)" 
-                                />
-                            </FormGroup>
-                            <FormGroup>
-                                <Input 
-                                type="text" 
-                                name="state" 
-                                placeholder="State (optional)" 
-                                />
-                            </FormGroup>
-                
-                             <FormGroup>
-                                <Label for="category-list">Category</Label>    
-                                 <Input 
-                                 type="select" 
-                                 name="select" 
-                                 id="category-list" 
-                                 placeholder="Select Business Category"
-                                >
-                                    <option>Select category</option>
-                                    <option>Mexican</option>
-                                    <option>Sea food</option>
-                                    <option>American Fusion</option>
-                                    <option>Chinese</option>
-                                    <option>Middle Eastern</option>
-                                    <option>Burgers</option>
-                                    <option>Italian</option>
-                                 </Input>
-                            </FormGroup>
-                        </Form>
-                        <Button color="success" onClick={this.submit}>Submit</Button>
+                    <Col sm="12">
+                        <Card>
+                            <CardHeader style={{ backgroundColor: 'rgb(242, 190, 0)' }}>Business Information </CardHeader>
+                            <CardBody>
+                                <Form>
+                                    <FormGroup>
+                                        <Input 
+                                        type="text" 
+                                        name="contact" 
+                                        id="contact" 
+                                        placeholder="Owner" 
+                                        
+                                        />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input 
+                                        type="text" 
+                                        name="name" 
+                                        id="business-name" 
+                                        placeholder="Business name"                                
+                                        />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input 
+                                        type="text" 
+                                        name="city" 
+                                        placeholder="City (optional)" 
+                                        />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input 
+                                        type="text" 
+                                        name="state" 
+                                        placeholder="State (optional)" 
+                                        />
+                                    </FormGroup>
+                        
+                                    <FormGroup>
+                                        <Label for="category-list">Category</Label>    
+                                        <Input 
+                                        type="select" 
+                                        name="select" 
+                                        id="category-list" 
+                                        placeholder="Select Business Category"
+                                        >
+                                            <option>Select category</option>
+                                            <option>Mexican</option>
+                                            <option>Sea food</option>
+                                            <option>American Fusion</option>
+                                            <option>Chinese</option>
+                                            <option>Middle Eastern</option>
+                                            <option>Burgers</option>
+                                            <option>Italian</option>
+                                        </Input>
+                                    </FormGroup>
+                                </Form>
+                                <Button color="success" onClick={this.submit}>Submit</Button>
+                            </CardBody>
+                        </Card>
                     </Col>
-                 </Row>
+                </Row>
             </Container>
         )
     }
