b14abb23c98a799278b89c2b68f7b7133f6f3f50
Raul Gonzalez <rgonz166@gmail.com>
2019-11-23 11:23:02 -0800

    Add: react-strap import to TwitterPost

--- client/src/App/components/TwitterPost/TwitterPost.js
+++ client/src/App/components/TwitterPost/TwitterPost.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react"
+import {Button,Form,FormGroup,Label,Input,Row,Col,Container,Card,CardBody,CardHeader} from "reactstrap"
 
 const TwitterBot = () => (
     <div className="">
