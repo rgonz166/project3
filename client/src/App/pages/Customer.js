424df62284c6de463199e340a432a2519d17b5b8
Mitu Fikir <hannatefera2@gmail.com>
2019-11-25 23:18:56 -0800

    Added MenuDisplay component

--- client/src/App/pages/Customer.js
+++ client/src/App/pages/Customer.js
@@ -2,23 +2,22 @@ import React, { Component } from 'react';
 import Searchbar from '../components/Searchbar/Searchbar';
 import GoogleApiWrapper from '../components/MapApp';
 import { Container, Row, Col } from 'reactstrap';
-
+import MenuDisplay from '../components/MenuDisplay';
 
 const Customer = () => {
-        return (
-            <Container className="mb-3">
+  return (
+    <Container className="mb-3">
       <Row>
         <Col lg={4}>
           <Searchbar/>
+          <MenuDisplay vendorId={"auth0%7C5dd0577c460ca50e25ac3769"} />
         </Col>
         <Col lg={8}>
-          <GoogleApiWrapper/>
-
+          <GoogleApiWrapper />
         </Col>
       </Row>
-      
     </Container>
-        )
+  )
 }
 
 export default Customer;
