a6bf6f3306d46e14bba9f5fe4a65f6aa8cb6969f
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-11-25 23:28:54 -0800

    vendor creation; menu creation; redirect; food addition

--- client/src/App/pages/BusinessInfo.js
+++ client/src/App/pages/BusinessInfo.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, Row, Col, Container, Card, CardBody, CardHeader } from 'reactstrap';
+import { Redirect } from "react-router-dom";
+import Category from '../components/Category';
+import API from '../utils/API';
 
 class BusinessInfo extends Component {
     constructor(props) {
@@ -10,103 +13,131 @@ class BusinessInfo extends Component {
             storeName: '',
             categories: '',
             city: '',
-            state: ''
+            state: '',
+            items: [],
+            status: 0
         }
     }
 
 
     componentDidMount() {
+        this.setState({ items: Category })
         console.log(this.state.auth0);
     }
 
-      handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
-    submit = (e) => {
+    handleFormSubmit = e => {
         e.preventDefault();
-        const newVendor = {
-            storeName: this.state.storeName,
-            owner: this.state.owner,
-            ownerId: this.state.auth0,
-            categories: [this.state.categories],
-            city: this.state.city ? this.state.city : '',
-            state: this.state.state ? this.state.state : '',
-        }
-
+        const menuObj = { menuName: this.state.storeName, food: [] };
+        API.createMenu(menuObj)
+            .then(reply => {
+                console.log("This is the reply: ", reply, reply.data._id);
+                console.log("Type of ID", typeof (reply.data._id));
+                let newVendor = {
+                    storeName: this.state.storeName,
+                    owner: this.state.owner,
+                    ownerId: this.state.auth0,
+                    categories: [this.state.categories],
+                    city: this.state.city ? this.state.city : '',
+                    state: this.state.state ? this.state.state : '',
+                    menu: reply.data._id
+                }
+                console.log("Object to be Submitted: ", newVendor);
+                API.createVendor(newVendor)
+                    .then(reply => {
+                        console.log(reply);
+                        this.setState({ status: 1 });
+                    }).catch(err => console.log("Vendor err: ", err));
+            }).catch(err => console.log(err));
     }
 
     render() {
-        const { } = this.props;
-        return (
-            <Container className="mb-3">
-                <Row>
-                    <Col sm="12">
-                        <Card>
-                            <CardHeader ><strong>Business Information</strong> </CardHeader>
-                            <CardBody>
-                                <Form>
-                                    <FormGroup>
-                                        <Input
-                                            type="text"
-                                            name="contact"
-                                            id="contact"
-                                            placeholder="Owner"
+        switch (this.state.status) {
+            case 0:
+                return (
+                    <Container className="mb-3">
+                        <Row>
+                            <Col sm="12">
+                                <Card>
+                                    <CardHeader ><strong>Business Information</strong> </CardHeader>
+                                    <CardBody>
+                                        <Form>
+                                            <FormGroup>
+                                                <Input
+                                                    type="text"
+                                                    name="owner"
+                                                    id="contact"
+                                                    onChange={this.handleInputChange}
+                                                    placeholder="Owner"
+                                                />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <Input
+                                                    type="text"
+                                                    name="storeName"
+                                                    id="business-name"
+                                                    onChange={this.handleInputChange}
+                                                    placeholder="Business name"
+                                                />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <Input
+                                                    type="text"
+                                                    name="city"
+                                                    onChange={this.handleInputChange}
+                                                    placeholder="City (optional)"
+                                                />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <Input
+                                                    type="text"
+                                                    name="state"
+                                                    onChange={this.handleInputChange}
+                                                    placeholder="State (optional)"
+                                                />
+                                            </FormGroup>
 
-                                        />
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Input
-                                            type="text"
-                                            name="name"
-                                            id="business-name"
-                                            placeholder="Business name"
-                                        />
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Input
-                                            type="text"
-                                            name="city"
-                                            placeholder="City (optional)"
-                                        />
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Input
-                                            type="text"
-                                            name="state"
-                                            placeholder="State (optional)"
-                                        />
-                                    </FormGroup>
-
-                                    <FormGroup>
-                                        <Label for="category-list">Category</Label>
-                                        <Input
-                                            type="select"
-                                            name="select"
-                                            id="category-list"
-                                            placeholder="Select Business Category"
+                                            <FormGroup>
+                                                <Label for="category-list">Category</Label>
+                                                <Input
+                                                    type="select"
+                                                    name="categories"
+                                                    id="category-list"
+                                                    onChange={this.handleInputChange}
+                                                    placeholder="Select Business Category"
+                                                >
+                                                    <option>Select category</option>
+                                                    {this.state.items.map(choice =>
+                                                        <option key={choice}>
+                                                            {choice}
+                                                        </option>
+                                                    )}
+                                                </Input>
+                                            </FormGroup>
+                                        </Form>
+                                        <Button
+                                            color="success"
+                                            disabled={!(this.state.owner && this.state.storeName)}
+                                            onClick={this.handleFormSubmit}
                                         >
-                                            <option>Select category</option>
-                                            <option>Mexican</option>
-                                            <option>Sea food</option>
-                                            <option>American Fusion</option>
-                                            <option>Chinese</option>
-                                            <option>Middle Eastern</option>
-                                            <option>Burgers</option>
-                                            <option>Italian</option>
-                                        </Input>
-                                    </FormGroup>
-                                </Form>
-                                <Button color="success" onClick={this.submit}>Submit</Button>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        )
+                                            Submit
+                                    </Button>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+                )
+            case 1:
+                return <Redirect to='/menu' />
+        }
+
     }
 }
 export default BusinessInfo;
