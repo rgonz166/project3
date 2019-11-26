0fba8afc45777a298c9e136d75d7ee25a9e44b55
Raul Gonzalez <rgonz166@gmail.com>
2019-11-26 14:04:04 -0800

    Removed: twitter api keys

--- client/src/App/utils/twitterConfig.js
+++ client/src/App/utils/twitterConfig.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 module.exports = {
-	consumer_key: "WsHheN3QgfhvaeAEPh8qJWf6W",
-	consumer_secret: "ZGJtWFAzA1PYaBvUWgitZRqEEQWLDrbflKVSTJLKjXg5BNn1zK",
-	access_token: "1192903746985947136-VLHJvfvr0mLYriOVDcloze6W9iLVfa",
-	access_token_secret: "SxsRBJXT7RhgYOHahjNzNW51TrEHDId7A9ctaf4Smt9cO"
+	consumer_key: "",
+	consumer_secret: "",
+	access_token: "",
+	access_token_secret: ""
 }
