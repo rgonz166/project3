3304981c2b48c626acfbc4d6ba0b0de7b39bf01f
Mitu Fikir <hannatefera2@gmail.com>
2019-11-26 00:15:30 -0800

    Modify landing and menu style.

--- client/src/App/pages/Customer.js
+++ client/src/App/pages/Customer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Searchbar from '../components/Searchbar/Searchbar';
 import GoogleApiWrapper from '../components/MapApp';
 import { Container, Row, Col } from 'reactstrap';
-import MenuDisplay from '../components/MenuDisplay';
 
 const Customer = () => {
   return (
@@ -10,7 +9,6 @@ const Customer = () => {
       <Row>
         <Col lg={4}>
           <Searchbar/>
-          <MenuDisplay vendorId={"auth0%7C5dd0577c460ca50e25ac3769"} />
         </Col>
         <Col lg={8}>
           <GoogleApiWrapper />
