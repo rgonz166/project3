74033e5ebfe4cf2859cbe654cc868415f98a244c
Raul Gonzalez <rgonz166@gmail.com>
2019-12-03 19:44:58 -0800

    Add: twitter posting script to post to twitter

--- controllers/twitterController.js
+++ controllers/twitterController.js
@@ -1,7 +1,18 @@
 // add twitter id to vendor object
+var Twit = require('twit');
+var T = new Twit({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
 
 module.exports = {
     postTweet: function (req, res) {
         console.log("posted",req.body.body);
+        T.post("statuses/update", { status: req.body.body }, function(err,data,response
+        ) {
+            console.log("Tweeted")
+        })
     }
 }
