68d4c351e9e06fae436d2e146aef6d4d82e951d2
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-12-03 19:45:25 -0800

    Saving to merge

--- client/src/App/components/Footer/index.js
+++ client/src/App/components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTruckMoving, faCopyright  } from '@fortawesome/free-solid-svg-icons'; 
-import './footer.css';
+import './Footer.css';
 
 const Footer = () => {
   return (
--- client/src/App/components/NavBar.js
+++ client/src/App/components/NavBar.js
@@ -58,6 +58,7 @@ const NavBar = () => {
 
             {/* Actually display on the screen */}
             <Nav className="d-none d-md-block" navbar>
+              {/* If the user is NOT logged in, display login button */}
               {!isAuthenticated && (
                 <NavItem>
                   <Button
@@ -70,6 +71,7 @@ const NavBar = () => {
                   </Button>
                 </NavItem>
               )}
+              {/* if the user IS logged in, display the circle drop down menu */}
               {isAuthenticated && (
                 <UncontrolledDropdown nav inNavbar>
                   <DropdownToggle nav caret id="profileDropDown">
@@ -122,6 +124,9 @@ const NavBar = () => {
                 </UncontrolledDropdown>
               )}
             </Nav>
+
+
+            {/* if the user is NOT logged in */}
             {!isAuthenticated && (
               <Nav className="d-md-none" navbar>
                 <NavItem>
